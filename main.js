2e7575d92a3df7af0b3ec164f6dc059bdae17fcf
sm <sumeriamahi#gmail.com>
2021-12-08 12:13:35 +0530

    smark

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function myFunction(){
+    document.getElementById('demo').innerHTML='gggggg';
+    document.getElementById('demo').style.color="blue";
+}
+function hhhh(){
+    document.getElementById('demo').innerHTML='submit';
+    document.getElementById('demo').style.color="green";
+}
+
+/*function myf(){
+    document.getElementById('ddd').innerHTML='click me';
+}*/
